d80883ff8a17b865361c51ca1513c8e6ebd73f47
codepeople <codepeople@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-24 12:46:37 +0000

    The current update corrects some issues of the plugin with the browsers Internet Explorer 9 and 10.
    git-svn-id: https://plugins.svn.wordpress.org/corner-ad/trunk@745649 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/cornerAd.js
+++ js/cornerAd.js
@@ -25,24 +25,72 @@
         
 		// Big corner 
 		toPrint +='<div id="bigCorner" style="z-index:100000;position:absolute;left:0px; top:0px;">';
-		toPrint +='	<object  id="cornerAd" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,0,0" width="500" height="500">';
-	  toPrint +='		<param name="movie" value="'+swfLocation+'?'+attrStr+'imgPath='+imgPath+'&isBig=true" />';
-	  toPrint +='		<param name="quality" value="high" />';
-	  toPrint +='		<param name="wmode" value="transparent" />';
-	  toPrint +='		<param name="allowScriptAccess" value="always" />';
-	  toPrint +='		<embed name="cornerAd" allowscriptaccess="always" src="'+swfLocation+'?'+attrStr+'imgPath='+imgPath+'&isBig=true" quality="high" wmode="transparent" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="500" height="500"></embed>';
-		toPrint +='	</object>';
+		toPrint +='<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="500" height="500" id="cornerAd" align="middle">\
+            <param name="movie" value="'+swfLocation+'?'+attrStr+'imgPath='+imgPath+'&isBig=true" />\
+            <param name="quality" value="high" />\
+            <param name="play" value="true" />\
+            <param name="loop" value="true" />\
+            <param name="wmode" value="transparent" />\
+            <param name="scale" value="exactfit" />\
+            <param name="menu" value="true" />\
+            <param name="devicefont" value="false" />\
+            <param name="salign" value="" />\
+            <param name="allowScriptAccess" value="sameDomain" />\
+            <!--[if !IE]>-->\
+            <object type="application/x-shockwave-flash" data="'+swfLocation+'" width="500" height="500">\
+                <param name="movie" value="'+swfLocation+'?'+attrStr+'imgPath='+imgPath+'&isBig=true" />\
+                <param name="quality" value="high" />\
+                <param name="play" value="true" />\
+                <param name="loop" value="true" />\
+                <param name="wmode" value="transparent" />\
+                <param name="scale" value="exactfit" />\
+                <param name="menu" value="true" />\
+                <param name="devicefont" value="false" />\
+                <param name="salign" value="" />\
+                <param name="allowScriptAccess" value="sameDomain" />\
+            <!--<![endif]-->\
+                <a href="http://www.adobe.com/go/getflash">\
+                    <img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Obtener Adobe Flash Player" />\
+                </a>\
+            <!--[if !IE]>-->\
+            </object>\
+            <!--<![endif]-->\
+        </object>';
 		toPrint +='</div>';
 		
 		// Short corner 
 		toPrint +='<div id="shortCorner" style="z-index:99999;position:absolute;left:0px; top:0px;">';
-		toPrint +='	<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,0,0" width="100" height="100">';
-	  toPrint +='		<param name="movie" value="'+swfLocation+'?'+attrStr+'imgPath='+thumbPath+'" />';
-	  toPrint +='		<param name="quality" value="high" />';
-	  toPrint +='		<param name="wmode" value="transparent" />';
-	  toPrint +='		<param name="allowScriptAccess" value="always" />';
-	  toPrint +='		<embed allowscriptaccess="always" src="'+swfLocation+'?'+attrStr+'imgPath='+thumbPath+'" quality="high" wmode="transparent" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="100" height="100"></embed>';
-		toPrint +='	</object>';
+		toPrint +='<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="100" height="100" align="middle">\
+            <param name="movie" value="'+swfLocation+'?'+attrStr+'imgPath='+thumbPath+'" />\
+            <param name="quality" value="high" />\
+            <param name="play" value="true" />\
+            <param name="loop" value="true" />\
+            <param name="wmode" value="transparent" />\
+            <param name="scale" value="exactfit" />\
+            <param name="menu" value="true" />\
+            <param name="devicefont" value="false" />\
+            <param name="salign" value="" />\
+            <param name="allowScriptAccess" value="sameDomain" />\
+            <!--[if !IE]>-->\
+            <object type="application/x-shockwave-flash" data="'+swfLocation+'" width="100" height="100">\
+                <param name="movie" value="'+swfLocation+'?'+attrStr+'imgPath='+thumbPath+'" />\
+                <param name="quality" value="high" />\
+                <param name="play" value="true" />\
+                <param name="loop" value="true" />\
+                <param name="wmode" value="transparent" />\
+                <param name="scale" value="exactfit" />\
+                <param name="menu" value="true" />\
+                <param name="devicefont" value="false" />\
+                <param name="salign" value="" />\
+                <param name="allowScriptAccess" value="sameDomain" />\
+            <!--<![endif]-->\
+                <a href="http://www.adobe.com/go/getflash">\
+                    <img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Obtener Adobe Flash Player" />\
+                </a>\
+            <!--[if !IE]>-->\
+            </object>\
+            <!--<![endif]-->\
+        </object>';
 		toPrint +='</div>';
 		
 		var body = document.getElementsByTagName('BODY');
