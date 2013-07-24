6fe23de31bb5d9824688359f3476b61eb51e15ba
codepeople <codepeople@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-24 14:03:35 +0000

    We have corrected other issues related to browsers compatibility.
    git-svn-id: https://plugins.svn.wordpress.org/corner-ad/tags/1.0.1@745687 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/cornerAd.js
+++ js/cornerAd.js
@@ -25,19 +25,10 @@
         
 		// Big corner 
 		toPrint +='<div id="bigCorner" style="z-index:100000;position:absolute;left:0px; top:0px;">';
-		toPrint +='<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="500" height="500" id="cornerAd" align="middle">\
-            <param name="movie" value="'+swfLocation+'?'+attrStr+'imgPath='+imgPath+'&isBig=true" />\
-            <param name="quality" value="high" />\
-            <param name="play" value="true" />\
-            <param name="loop" value="true" />\
-            <param name="wmode" value="transparent" />\
-            <param name="scale" value="exactfit" />\
-            <param name="menu" value="true" />\
-            <param name="devicefont" value="false" />\
-            <param name="salign" value="" />\
-            <param name="allowScriptAccess" value="sameDomain" />\
-            <!--[if !IE]>-->\
-            <object type="application/x-shockwave-flash" data="'+swfLocation+'" width="500" height="500">\
+        if (navigator.appName.indexOf("Microsoft") == -1) {
+            toPrint +='<embed name="cornerAd" allowscriptaccess="always" src="'+swfLocation+'?'+attrStr+'imgPath='+imgPath+'&isBig=true" quality="high" wmode="transparent" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="500" height="500"></embed>';
+		}else{
+            toPrint +='<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="500" height="500" id="cornerAd" align="middle">\
                 <param name="movie" value="'+swfLocation+'?'+attrStr+'imgPath='+imgPath+'&isBig=true" />\
                 <param name="quality" value="high" />\
                 <param name="play" value="true" />\
@@ -48,31 +39,16 @@
                 <param name="devicefont" value="false" />\
                 <param name="salign" value="" />\
                 <param name="allowScriptAccess" value="sameDomain" />\
-            <!--<![endif]-->\
-                <a href="http://www.adobe.com/go/getflash">\
-                    <img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Obtener Adobe Flash Player" />\
-                </a>\
-            <!--[if !IE]>-->\
-            </object>\
-            <!--<![endif]-->\
-        </object>';
+            </object>';
+        }    
 		toPrint +='</div>';
 		
 		// Short corner 
 		toPrint +='<div id="shortCorner" style="z-index:99999;position:absolute;left:0px; top:0px;">';
-		toPrint +='<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="100" height="100" align="middle">\
-            <param name="movie" value="'+swfLocation+'?'+attrStr+'imgPath='+thumbPath+'" />\
-            <param name="quality" value="high" />\
-            <param name="play" value="true" />\
-            <param name="loop" value="true" />\
-            <param name="wmode" value="transparent" />\
-            <param name="scale" value="exactfit" />\
-            <param name="menu" value="true" />\
-            <param name="devicefont" value="false" />\
-            <param name="salign" value="" />\
-            <param name="allowScriptAccess" value="sameDomain" />\
-            <!--[if !IE]>-->\
-            <object type="application/x-shockwave-flash" data="'+swfLocation+'" width="100" height="100">\
+        if (navigator.appName.indexOf("Microsoft") == -1) {
+            toPrint +='<embed allowscriptaccess="always" src="'+swfLocation+'?'+attrStr+'imgPath='+thumbPath+'" quality="high" wmode="transparent" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="100" height="100"></embed>';
+        }else{
+            toPrint +='<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="100" height="100" align="middle">\
                 <param name="movie" value="'+swfLocation+'?'+attrStr+'imgPath='+thumbPath+'" />\
                 <param name="quality" value="high" />\
                 <param name="play" value="true" />\
@@ -83,14 +59,8 @@
                 <param name="devicefont" value="false" />\
                 <param name="salign" value="" />\
                 <param name="allowScriptAccess" value="sameDomain" />\
-            <!--<![endif]-->\
-                <a href="http://www.adobe.com/go/getflash">\
-                    <img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Obtener Adobe Flash Player" />\
-                </a>\
-            <!--[if !IE]>-->\
-            </object>\
-            <!--<![endif]-->\
-        </object>';
+            </object>';
+        }    
 		toPrint +='</div>';
 		
 		var body = document.getElementsByTagName('BODY');
@@ -120,15 +90,15 @@
 						bigMovie.style.top = '0px';
 						shortMovie.style.top = '-1000px';
 						
-						if (navigator.appName.indexOf("Microsoft") != -1) {
-	        		ad = window['cornerAd'];
-		    		} else {
-		        	ad = document['cornerAd'];
-		    		}
+                        if (navigator.appName.indexOf("Microsoft") != -1) {
+                            ad = window['cornerAd'];
+                        } else {
+                            ad = document['cornerAd'];
+                        }
 						
-						if(ad){
-							ad.nextStep();
-						}
+                        if(ad){
+                            ad.nextStep();
+                        }
 					}else{
 						bigMovie.style.top = '-1000px';
 						shortMovie.style.top = '0px';
